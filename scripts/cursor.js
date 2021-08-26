58aa9492d378e67d7cf3c7013a6f962181069645
Kiosion <hi@kio.dev>
2021-08-26 03:21:14 -0300

    Update example page

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,64 +1,63 @@
 const cursor = document.querySelector("#cursor");
-function bindToQuerySelector(querySelector) {
-    var a = document.querySelectorAll(querySelector);
+const bindToQuerySelector = (querySelector) => {
+    let a = document.querySelectorAll(querySelector);
     a.forEach(e => e.addEventListener('mouseenter', handleMouseEnter));
     a.forEach(e => e.addEventListener('mouseleave', handleMouseLeave));
 }
+
 //Follow Cursor
-var $mouseX = 0, $mouseY = 0;
-var $xp = 0, $yp =0;
+let mouseX = 0, mouseY = 0;
+let xp = 0, yp =0;
 cursor.style.display = "none";
-$(document).mousemove(function(e){
-    $mouseX = e.pageX;
-    $mouseY = e.pageY;
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
 });
 
-var $loop = setInterval(function(){
-    $xp += (($mouseX - $xp)/6);
-    $yp += (($mouseY - $yp)/6);
-    $("#cursor").css({left:$xp +'px', top:$yp +'px'});
+let loop = setInterval(() => {
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    cursor.style.left = xp + 'px';
+    cursor.style.top = yp + 'px';
 }, 6);
 
 //Hide if mobile device
 if(navigator.userAgent.toLowerCase().match(/mobile/i)) {
     cursor.style.display = "none";
-    return;
-}
-else {
-    document.addEventListener("mousemove", cursorOnMove)
-
-    function cursorOnMove() {
-    	cursor.style.display = "block";
-      	document.removeEventListener("mousemove", cursorOnMove);
+} else {
+    const cursorOnMove = () => {
+        cursor.style.display = "block";
+        document.removeEventListener("mousemove", cursorOnMove);
     }
+    document.addEventListener("mousemove", cursorOnMove)
 }
 
 // event: mouseout
 document.addEventListener("mouseout", () => {
     cursor.style.display = "none";
-})
+});
 
 // event: mouseover
 document.addEventListener("mouseover", () => {
     cursor.style.display = "block";
-})
+});
 
 // event: mousedown
 document.addEventListener("mousedown", () => {
     cursor.classList.add('click');
-})
+});
 
 // event: mouseup
 document.addEventListener("mouseup", () => {
     cursor.classList.remove('click');
-})
+});
 
 // event: mouse enter on link
-function handleMouseEnter() {
+const handleMouseEnter = () => {
     cursor.classList.add('hovered');
 }
 
 // event: mouse leave on link
-function handleMouseLeave() {
+const handleMouseLeave = () => {
     cursor.classList.remove('hovered');
 }
